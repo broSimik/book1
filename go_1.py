6fd1cdde6a957fa807faa29b4d33f5147b2ecafd
simik <simik101297@gmail.com>
2023-03-12 06:21:46 +0600

    commit

--- go_1.py
+++ go_1.py
@@ -11,3 +11,18 @@ print(liq)
 liq.pop()
 
 print(liq)
+
+M = [[1, 2, 3],
+     [4, 5, 6],
+     [7, 8, 9]]
+
+print(M[1])
+print(M[1][2])
+
+supern = [row[0] for row in M]
+superM = [row[1] for row in M]
+superMMM = [row[2] for row in M]
+
+print(supern)
+print(superM)
+print(superMMM)
